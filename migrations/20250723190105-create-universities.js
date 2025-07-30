002e4f16314596d99291872361a8b23d4e65a65c
hallsarturo <proalarturo@gmail.com>
2025-07-30 10:32:47 -0600

    added db baseline.sql file for db schema recreation.

--- migrations/20250723190105-create-universities.js
+++
@@ -1,9 +0,0 @@
-export async function up(queryInterface, Sequelize) {
-    await queryInterface.createTable('universities', {
-        id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
-        name: { type: Sequelize.STRING, allowNull: false, unique: true },
-    });
-}
-export async function down(queryInterface) {
-    await queryInterface.dropTable('universities');
-}
--- migrations/20250724153931-changed-comments-to-posts_comments.js
+++
@@ -1,7 +0,0 @@
-export async function up(queryInterface, Sequelize) {
-    await queryInterface.renameTable('comments', 'post_comments');
-}
-
-export async function down(queryInterface, Sequelize) {
-    await queryInterface.renameTable('post_comments', 'comments');
-}
