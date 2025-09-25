6ae2cca5cc78e8403082b4adb52435e42b1dcae0
hallsarturo <proalarturo@gmail.com>
2025-09-25 09:44:23 -0600

    update CORS middleware to allow DELETE requests

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.use(
     cors({
         origin: process.env.FRONTEND_URL,
         credentials: true,
-        methods: ['GET', 'POST', 'PUT'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
     })
 );
